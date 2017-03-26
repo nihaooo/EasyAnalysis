cdef0226a7ecfff74a30e6d092e1b8dc62b0eafd
qzhuorui <qzhuorui@163.com>
2017-03-26 18:01:18 +0800

    完成线性图标，成功运行

--- app/src/main/java/com/easyanalysis/android/ChartActivity.java
+++ app/src/main/java/com/easyanalysis/android/ChartActivity.java
@@ -21,15 +21,16 @@ import lecho.lib.hellocharts.view.LineChartView;
 
 public class ChartActivity extends Activity{
     private LineChartView mchart;
-    private Map<String,Integer> table = new TreeMap<>();
+    private Map<String,Integer> table = new TreeMap<>();//和坐标排序
     private LineChartData mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chart_view);
         mchart = (LineChartView) findViewById(R.id.chart);
-        List<CostBean> allDate = (List<CostBean>) getIntent().getSerializableExtra("cost_list");
-        generateValue(allDate);
+        List<CostBean> allDate = (List<CostBean>) getIntent().getSerializableExtra("cost_list");//取出数据
+        //对数据进行处理，如果有同一天则合并
+        generateValue(allDate);//此时table是数据源
         generateData();
         mchart.setLineChartData(mData);
     }
@@ -52,16 +53,16 @@ public class ChartActivity extends Activity{
         mData = new LineChartData(lines);
     }
 
-    private void generateValue(List<CostBean> allDate) {
+    private void generateValue(List<CostBean> allDate) {//重复数据累加，不重复收集到数据源中table
         if (allDate != null) {
-            for (int i =0;i<allDate.size();i++) {
+            for (int i =0;i<allDate.size();i++) {//每一二个都是costbean类型
                 CostBean costBean = allDate.get(i);
-                String costDate = costBean.costDate;
-                int costMoney = Integer.parseInt(costBean.costMoney);
+                String costDate = costBean.costDate;//日期
+                int costMoney = Integer.parseInt(costBean.costMoney);//金额
                 if (!table.containsKey(costDate)) {
                     table.put(costDate,costMoney);
                 }else {
-                    int originMoney = table.get(costDate);
+                    int originMoney = table.get(costDate);//取出原始值
                     table.put(costDate,originMoney + costMoney);//table是数据源
                 }
             }
